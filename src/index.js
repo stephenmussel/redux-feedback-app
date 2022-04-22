d77094bb95b6decbeee1807d8cc672ceeda5d905
phaydara <phaydara@gmail.com>
2022-04-22 15:56:46 -0500

    set up ReduxStore

--- src/index.js
+++ src/index.js
@@ -4,5 +4,30 @@ import './index.css';
 import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// provides store to App.jsx
+import { Provider } from 'react-redux';
+
+// setting up store and logger
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import logger from 'redux-logger';
+
+// TODO add more reducers?
+const feedbackReducer = (state = [], action) => {
+    console.log('in feedbackReducer');
+    return state;
+}
+
+// creates store
+const ReduxStore = createStore(
+    combineReducers({
+        feedbackReducer,
+    }),
+    applyMiddleware(logger)
+);
+
+ReactDOM.render(
+    <Provider store={ReduxStore}>
+        <App />
+    </Provider>, 
+document.getElementById('root'));
 registerServiceWorker();
