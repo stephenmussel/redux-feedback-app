25b9286630c0b87614898296e10e9d47a81b15dd
phaydara <phaydara@gmail.com>
2022-04-22 16:30:27 -0500

    captured input and click event

--- src/components/FeelingForm/FeelingForm.jsx
+++ src/components/FeelingForm/FeelingForm.jsx
@@ -1,7 +1,32 @@
+import { useState } from 'react';
+
 function FeelingForm() {
+
+    const [feeling, setFeeling] = useState('');
+
+    const handleFeelingSubmit = () => {
+        console.log('in handleFeelingSubmit');
+        console.log(feeling);
+    }
+
     return(
         <>
             <h1>How are you feeling?</h1>
+            <form onSubmit={handleFeelingSubmit}>
+                <input
+                    type="number"
+                    min="1"
+                    max="5"
+                    style={{width: 50}}
+                    placeholder="1-5"
+                    onChange={event => setFeeling(event.target.value)}
+                />
+                <input 
+                    type="submit" 
+                    value="Next" 
+                    style={{margin: 5}}
+                />
+            </form>
         </>
     )
 }
