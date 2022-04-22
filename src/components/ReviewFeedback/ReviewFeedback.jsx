86a03ecbc9d72176bfa09a1851ac60f493561bee
phaydara <phaydara@gmail.com>
2022-04-22 18:20:40 -0500

    splat on DOM

--- src/components/ReviewFeedback/ReviewFeedback.jsx
+++ src/components/ReviewFeedback/ReviewFeedback.jsx
@@ -1,7 +1,13 @@
+import { useSelector } from 'react-redux';
+
 function ReviewFeedback() {
+
+    const feedback = useSelector(store => store.feedbackReducer);
+
     return(
         <>
             <h1>Let's review your feedback: </h1>
+            <p>{JSON.stringify(feedback)}</p>
         </>
     )
 }
