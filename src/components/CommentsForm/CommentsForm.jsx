d1f9b53c641fd57ddf669d9a95e4c8a655192f8c
phaydara <phaydara@gmail.com>
2022-04-22 18:08:34 -0500

    captured input and click event

--- src/components/CommentsForm/CommentsForm.jsx
+++ src/components/CommentsForm/CommentsForm.jsx
@@ -1,7 +1,38 @@
+import { useState } from 'react';
+
 function CommentsForm() {
+
+    const [comments, setComments] = useState('');
+
+    const feedbackObject = {
+        comments: comments,
+    }
+
+    const handleCommentsSubmit = (event) => {
+        event.preventDefault();
+
+        console.log('in handleCommentsSubmit');
+        console.log('comments: ', comments);
+        console.log('feedbackObject: ', feedbackObject);
+    }
+
     return(
         <>
             <h1>Any comments you want to leave?</h1>
+            <form onSubmit={handleCommentsSubmit}>
+                <textarea
+                    required
+                    rows={10}
+                    cols={50}
+                    onChange={event => setComments(event.target.value)}
+                    value={comments}
+                 /><br />
+                <input 
+                    type="submit"
+                    value="Next"
+                    style={{margin: 5}}
+                />
+            </form>
         </>
     )
 }
