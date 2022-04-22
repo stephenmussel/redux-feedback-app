48f4f07f201073e018c778ed7e238fb190741ebf
phaydara <phaydara@gmail.com>
2022-04-22 17:22:55 -0500

    captured understanding input and click event

--- src/components/UnderstandingForm/UnderstandingForm.jsx
+++ src/components/UnderstandingForm/UnderstandingForm.jsx
@@ -1,7 +1,41 @@
+import { useState } from 'react';
+
 function UnderstandingForm() {
+
+    const [understanding, setUnderstanding] = useState('');
+
+    const feedbackObject = {
+        understanding: understanding,
+    }
+
+    const handleUnderstandSubmit = (event) => {
+        event.preventDefault();
+        
+        console.log('in handleUnderstandSubmit');
+        console.log('understanding: ', understanding);
+        console.log('feedbackObject: ', feedbackObject);
+        
+    }
+
     return(
         <>
             <h1>How well are you understanding the content?</h1>
+            <form onSubmit={handleUnderstandSubmit}>
+                <input
+                    type="number"
+                    min="1"
+                    max="5"
+                    style={{width: 50}}
+                    placeholder="1-5"
+                    onChange={event => setUnderstanding(event.target.value)}
+                    value={understanding}
+                />
+                <input
+                    type="submit"
+                    value="Next"
+                    style={{margin: 5}}
+                />
+            </form>
         </>
     )
 }
