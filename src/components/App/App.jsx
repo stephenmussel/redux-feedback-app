7c9611e161388966f28c5a0b508bf7ae16e2a8a1
phaydara <phaydara@gmail.com>
2022-04-22 15:03:50 -0500

    moved FeelingForm into component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import axios from 'axios';
 import './App.css';
 import Header from '../Header/Header';
+import FeelingForm from '../FeelingForm/FeelingForm';
 
 function App() {
 
   return (
     <div className='App'>
       <Header />
+      <FeelingForm />
     </div>
   );
 }
---
+++ src/components/FeelingForm/FeelingForm.jsx
@@ -0,0 +1,9 @@
+function FeelingForm() {
+    return(
+        <>
+            <h1>How are you feeling?</h1>
+        </>
+    )
+}
+
+export default FeelingForm;
