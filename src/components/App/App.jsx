572b6462df2792f5dc4a1d4ab1ab3f46ee1ed456
phaydara <phaydara@gmail.com>
2022-04-22 15:33:13 -0500

    added routes for each component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import axios from 'axios';
 import './App.css';
+
 import Header from '../Header/Header';
 import FeelingForm from '../FeelingForm/FeelingForm';
 import UnderstandingForm from '../UnderstandingForm/UnderstandingForm';
@@ -9,17 +10,22 @@ import CommentsForm from '../CommentsForm/CommentsForm';
 import ReviewFeedback from '../ReviewFeedback/ReviewFeedback';
 import ThankYou from '../ThankYou/ThankYou';
 
+import { HashRouter as Router, Route } from 'react-router-dom';
+
 function App() {
 
   return (
     <div className='App'>
       <Header />
-      <FeelingForm />
-      <UnderstandingForm />
-      <SupportedForm />
-      <CommentsForm />
-      <ReviewFeedback />
-      <ThankYou />
+      <Router>
+        <Route exact path="/" component={FeelingForm} />
+        <Route exact path="/understanding" component={UnderstandingForm}/>
+        <Route exact path="/supported" component={SupportedForm}/>
+        <Route exact path="/comments" component={CommentsForm}/>
+        <Route exact path="/review" component={ReviewFeedback}/>
+        <Route exact path="/thankyou" component={ThankYou}/>
+      </Router>
+     
     </div>
   );
 }
