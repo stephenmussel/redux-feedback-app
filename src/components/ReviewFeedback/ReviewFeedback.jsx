c92fb3b9de01e4543163f6ff2575152126799414
phaydara <phaydara@gmail.com>
2022-04-23 18:36:46 -0500

    added client-side POST request

--- src/components/ReviewFeedback/ReviewFeedback.jsx
+++ src/components/ReviewFeedback/ReviewFeedback.jsx
@@ -1,16 +1,28 @@
 import { useSelector } from 'react-redux';
 import axios from 'axios';
+import { useHistory } from 'react-router-dom';
 
 function ReviewFeedback() {
 
     const feedback = useSelector(store => store.feedbackReducer);
+    const history = useHistory();
 
-    // TODO POST to DB
+    // TODO POST to DB: add client POST request then add server side route and router
     // TODO Submission success page w/button to take new survey and clears data in store
     const handleReviewSubmit = (event) => {
         event.preventDefault();
 
         console.log('in handleReviewSubmit');
+
+        // POSTs feedback to DB then redirects to success page
+        axios({
+            method: 'POST',
+            url: '/feedback',
+            data: feedback
+        }).then(response => {
+            console.log('feedback: ', feedback);
+            history.pushState('/thankyou');
+        })
     }
 
     return(
