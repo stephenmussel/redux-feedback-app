84bd1dddbc49a1afc5053c8345061cbd14be1d2a
phaydara <phaydara@gmail.com>
2022-04-24 20:54:25 -0500

    displayed feedback results from DB in table

--- src/components/Admin/Admin.jsx
+++ src/components/Admin/Admin.jsx
@@ -30,7 +30,30 @@ function Admin() {
         
         <>
             <h1>Admin</h1>
-            <p>{JSON.stringify(results)}</p>
+            {/* <p>{JSON.stringify(results)}</p> */}
+            <table>
+                <thead>
+                    <tr>
+                        <th>Feeling</th>
+                        <th>Understanding</th>
+                        <th>Support</th>
+                        <th>Comments</th>
+                        <th>Delete</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {results.map(each => (
+                    <tr key={each.id}>
+                        <td>{each.feeling}</td>
+                        <td>{each.understanding}</td>
+                        <td>{each.support}</td>
+                        <td>{each.comments}</td>
+                        <td><button>Delete</button></td>
+                    </tr>
+                    ))}
+                    
+                </tbody>
+            </table>
         </>
     )
 }
