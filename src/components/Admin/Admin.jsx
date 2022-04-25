0b96e7b73d6e7126f34c76df288cab3b8100fb2d
phaydara <phaydara@gmail.com>
2022-04-24 20:11:10 -0500

    added client GET request

--- src/components/Admin/Admin.jsx
+++ src/components/Admin/Admin.jsx
@@ -1,4 +1,30 @@
+import { useState, useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import axios from 'axios';
+
 function Admin() {
+
+    const [results, setResults] = useState([]);
+    const feedback = useSelector(store => store.feedbackReducer);
+
+    useEffect(() => {
+        console.log('in admin useEffect');
+        fetchFeedback();
+    }, [])
+
+    const fetchFeedback = () => {
+        console.log('in fetchFeedback');
+        axios({
+            method: 'GET',
+            url: '/feedback',
+        }).then(response => {
+            console.log('response.data: ', response.data);
+            setResults(response.data)
+        }).catch(error => {
+            console.log('error in GET', error);
+        })
+    }
+
     return(
         <>
             <h1>Admin</h1>
