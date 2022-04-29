290780782b1dc541780231b9c574029624bb1d1c
phaydara <phaydara@gmail.com>
2022-04-29 13:24:15 -0500

    updated input validation for supportForm

--- src/components/SupportForm/SupportForm.jsx
+++ src/components/SupportForm/SupportForm.jsx
@@ -31,11 +31,15 @@ function SupportForm() {
         console.log('support: ', support);
         console.log('feedbackObject: ', feedbackObject);
 
-        const action = {type: 'ADD_SUPPORT', payload: feedbackObject};
-        dispatch(action);
-        setSupport('');
-        nextComments();
-    }
+        if(support === "" || support < 1 || support > 5) {
+            alert('Please enter a number between 1 and 5.')
+        } else {
+            const action = {type: 'ADD_SUPPORT', payload: feedbackObject};
+            dispatch(action);
+            setSupport('');
+            nextComments();
+        };
+    };
 
     const nextComments = () => {
         history.push('/comments');
@@ -54,20 +58,12 @@ function SupportForm() {
             <h1>How well are you being supported?</h1>
             <form onSubmit={handleSupportSubmit}>
                 <input 
-                    required
                     type="number"
-                    min="1"
-                    max="5"
                     placeholder="1-5"
                     style={{width: 50}}
                     onChange={event => setSupport(event.target.value)}
                     value={support}
                 />
-                {/* <input 
-                    type="submit"
-                    value="Next"
-                    style={{margin: 5}}
-                /> */}
                 <Button sx={{ml:1, mr: 1}} variant="contained" onClick={goBack} color="secondary">Back</Button>
                 <Button variant="contained" type="submit">Next</Button>
             </form>
